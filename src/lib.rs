6a35f1cd508048e8047e098f446f08d69da4d002
KmolYuan <pyslvs@gmail.com>
2023-01-29 00:04:05 +0800

    Reexport dependencies.

--- src/lib.rs
+++ src/lib.rs
@@ -29,7 +29,8 @@
 extern crate alloc;
 #[cfg(not(feature = "std"))]
 extern crate core as std; // for `ndarray::s!` macro
-extern crate nalgebra as na;
+pub extern crate nalgebra as na;
+pub extern crate ndarray;
 
 pub use crate::{efd::*, efd_dim::*, error::*, transform::*, utility::*};
 
