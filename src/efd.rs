914484ba1bc3966a49029cb143a6538affe4ba69
KmolYuan <pyslvs@gmail.com>
2023-03-12 20:45:10 +0800

    Code improvement.

--- src/efd.rs
+++ src/efd.rs
@@ -1,6 +1,6 @@
 use crate::*;
 use core::{f64::consts::TAU, marker::PhantomData};
-use ndarray::{s, Array1, Array2, Axis};
+use ndarray::{s, Array1, Array2, Axis, Slice};
 #[cfg(not(feature = "std"))]
 use num_traits::Float as _;
 
@@ -62,24 +62,7 @@ impl<D: EfdDim> Efd<D> {
         C: Into<CowCurve<'a, D::Trans>>,
         T: Into<Option<f64>>,
     {
-        let curve = curve.into();
-        let threshold = threshold.into().unwrap_or(0.9999);
-        assert!((0.0..1.).contains(&threshold));
-        if curve.len() < 2 {
-            return None;
-        }
-        // Nyquist Frequency
-        let harmonic = curve.len() / 2;
-        let (coeffs, _) = D::from_curve_harmonic(curve, harmonic);
-        let lut = cumsum(coeffs.mapv(pow2), None).sum_axis(Axis(1));
-        let total_power = lut.last().unwrap();
-        let harmonic = lut
-            .iter()
-            .enumerate()
-            .find(|(_, power)| *power / total_power >= threshold)
-            .map(|(i, _)| i + 1)
-            .unwrap_or(harmonic);
-        Some(harmonic)
+        Self::from_curve_gate(curve, threshold).map(|efd| efd.harmonic())
     }
 
     /// Calculate EFD coefficients from an existing discrete points.
@@ -108,8 +91,23 @@ impl<D: EfdDim> Efd<D> {
         T: Into<Option<f64>>,
     {
         let curve = curve.into();
-        let harmonic = Self::gate(curve.as_ref(), threshold)?;
-        Self::from_curve_harmonic(curve, harmonic)
+        if curve.len() < 2 {
+            return None;
+        }
+        let threshold = threshold.into().unwrap_or(0.9999);
+        // Nyquist Frequency
+        let harmonic = curve.len() / 2;
+        let (mut coeffs, trans) = D::from_curve_harmonic(curve, harmonic);
+        let lut = cumsum(coeffs.mapv(pow2), None).sum_axis(Axis(1));
+        let total_power = lut.last().unwrap();
+        let harmonic = lut
+            .iter()
+            .enumerate()
+            .find(|(_, power)| *power / total_power >= threshold)
+            .map(|(i, _)| i + 1)
+            .unwrap_or(coeffs.nrows());
+        coeffs.slice_axis_inplace(Axis(0), Slice::from(..harmonic));
+        Some(Self { coeffs, trans, _dim: PhantomData })
     }
 
     /// Calculate EFD coefficients from a series of existing discrete points.
@@ -129,16 +127,17 @@ impl<D: EfdDim> Efd<D> {
         C: Into<CowCurve<'a, D::Trans>>,
         H: Into<Option<usize>>,
     {
-        let curve = curve.into();
-        let harmonic = harmonic
-            .into()
-            .or_else(|| Self::gate(curve.as_ref(), None))?;
-        assert!(harmonic > 0);
-        if curve.len() < 2 {
-            return None;
+        if let Some(harmonic) = harmonic.into() {
+            let curve = curve.into();
+            if curve.len() < 2 {
+                None
+            } else {
+                let (coeffs, trans) = D::from_curve_harmonic(curve, harmonic);
+                Some(Self { coeffs, trans, _dim: PhantomData })
+            }
+        } else {
+            Self::from_curve(curve)
         }
-        let (coeffs, trans) = D::from_curve_harmonic(curve, harmonic);
-        Some(Self { coeffs, trans, _dim: PhantomData })
     }
 
     /// A builder method for changing transform type.
