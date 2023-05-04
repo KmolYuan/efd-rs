3e39c845f05fe4148efd2436461d78184f1f6cce
KmolYuan <pyslvs@gmail.com>
2023-05-04 13:17:49 +0800

    Fix curve generation.

--- src/efd.rs
+++ src/efd.rs
@@ -269,9 +269,7 @@ impl<D: EfdDim> Efd<D> {
     #[must_use]
     pub fn generate_norm_in(&self, n: usize, theta: f64) -> Vec<Coord<D>> {
         assert!(n > 1, "n ({n}) must larger than 1");
-        let mut t = na::Matrix1xX::repeat(n, 1. / (n - 1) as f64);
-        t[0] = 0.;
-        let t = cumsum(t) * theta;
+        let t = na::Matrix1xX::from_fn(n, |_, i| i as f64 / n as f64 * theta);
         self.coeffs
             .column_iter()
             .enumerate()
